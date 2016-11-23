8c5fb4964a0382bb7358b69235b3e1b710f104c6
HTML CSS tanártovábbképzés <htmlcss@natt.lcl>
2016-11-23 19:45:53 +0100

    dynamic student html

--- js/osztaly.js
+++ js/osztaly.js
@@ -12,10 +12,35 @@ var student = function(settings) {
   }
 };
 
+// CSV to JSON
+function csvToJson(csv) {
+    // 
+}
+
+// Tanuló köszönése.
+var students = [];
+function greetStudent(index) {
+    alert(students[index].greeting('tanár'));
+}
+
+// Popover üzenetek beállítása.
+function setPopovers() {
+    var buttons = document.querySelectorAll('button[data-student]');
+    for (var i = 0; i < buttons.length; i++) {
+        var index = buttons[i].getAttribute('data-student');
+        index = parseInt(index);
+        $(buttons[i]).popover({
+            container: 'body',
+            content: students[index].greeting('tanár'),
+            trigger: 'focus',
+            placement: 'top'
+        });
+    }
+};
+
 // Student json beolvasása.
-$.getJSON('/js/students.json', function(studentDB) {
+$.getJSON('/js/students.csv', function(studentDB) {
     // Tanulók példányosítása.
-    var students = [];
     for(var k in studentDB.students) {
         students.push( new student(studentDB.students[k]) );
     }
@@ -24,4 +49,39 @@ $.getJSON('/js/students.json', function(studentDB) {
     document.querySelector('.navbar-inverse .navbar-brand')
         .innerHTML = studentDB.className + " osztály";
     
+    // Osztályfőnök beállítása.
+    document.querySelector('.class-master')
+        .innerHTML = studentDB.classMaster;
+    
+    // Osztály létszám.
+    document.querySelector('.class-members')
+        .innerHTML = students.length;
+    
+    // Tanulók kilistázása.
+    var tBody = document.querySelector('.dynamic-table tbody');
+    var tBodyContent = '';
+    for(var k in students) {
+        tBodyContent += '<tr>';
+        tBodyContent += '<td>'+(parseInt(k)+1)+'.</td>';
+        tBodyContent += '<td>'+(students[k].name)+'.</td>';
+        tBodyContent += '<td>'+(students[k].age)+'.</td>';
+        tBodyContent += '<td>'+(students[k].myClass)+'.</td>';
+        tBodyContent += '<td><button class="btn btn-info" data-student="' + 
+            k + '">köszön</button></td>';
+        tBodyContent += '</tr>';
+    }
+    tBody.innerHTML = tBodyContent;
+    
+    // Popover beállítása.
+    setPopovers();
+    
 });
+
+
+
+
+
+
+
+
+
